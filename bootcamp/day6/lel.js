bfd4430c7806a59b24345823b044d5cbe5a4dc7e
mengeroshi <dpllgr@hotmail.com>
2020-04-18 23:50:21 -0500

    js execerises

---
+++ bootcamp/day6/lel.js
@@ -0,0 +1,17 @@
+let obos = "HolaMUNDO";
+
+console.log(obos.substr(0, 3));
+
+console.log(Math.max(3, 3));
+z
+let list = [5,4,4,3, 5,6,7, 60]
+
+
+console.log(list.sort((a, b)=> a - b));
+
+let obo = [
+{ name: "Some Person", jobTitle: "Boss Person", age: 30 },
+{ name: "xxxx", jobTitle: "Boss Person", age: 30 },
+{ name: "yyyy", jobTitle: "Boss Person", age: 30 },
+];
+console.log(obo.map((person)=> person.name));
