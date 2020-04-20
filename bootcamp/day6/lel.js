e5b1ad15de100c475caa4936c84103f36a2a20f9
mengeroshi <dpllgr@hotmail.com>
2020-04-19 23:55:59 -0500

    test in jasmine passed

--- bootcamp/day6/lel.js
+++ bootcamp/day6/lel.js
@@ -3,7 +3,7 @@ let obos = "HolaMUNDO";
 console.log(obos.substr(0, 3));
 
 console.log(Math.max(3, 3));
-z
+
 let list = [5,4,4,3, 5,6,7, 60]
 
 
@@ -12,6 +12,10 @@ console.log(list.sort((a, b)=> a - b));
 let obo = [
 { name: "Some Person", jobTitle: "Boss Person", age: 30 },
 { name: "xxxx", jobTitle: "Boss Person", age: 30 },
-{ name: "yyyy", jobTitle: "Boss Person", age: 30 },
+{ name: "yyyy", jobTitle: "Boss Person", age: 20 },
 ];
 console.log(obo.map((person)=> person.name));
+
+const ages =  obo.map((person)=> person.age);
+console.log(ages.reduce((a, b)=> a + b));
+let average = ages.reduce((a, b)=> a + b) / ages.length
