1e5cd5a20332629dd2c3acaca9c40954211851e3
mengeroshi <dpllgr@hotmail.com>
2020-04-25 23:15:52 -0500

    galley1 in vanilla added and gallery2 with swiper added

---
+++ bootcamp/day7/gallery2/my-gallery.js
@@ -0,0 +1,10 @@
+new Swiper('.swiper-container',{
+    speed: 400,
+    spaceBetween: 100,
+    navigation:{
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    effect: "flip",
+    loop: true,
+});  
